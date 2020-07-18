7026af3e3bd958aca4ca86599797a8598a0cec7d
loshy <loshy244110@gmail.com>
2020-07-18 21:45:59 +0900

    feat: LIBRARIES IN DENO
    - using dtae_fns

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import { serve } from "https://deno.land/std/http/server.ts";
+import format from "https://deno.land/x/date_fns/format/index.js";
 
 type StoryT = {
   title: string;
@@ -16,7 +17,11 @@ for await (const req of server) {
   const stories = result.hits.map((hit: StoryT) => ({
     title: hit.title,
     url: hit.url,
-    createdAt: hit.created_at_i,
+    createdAt: format(
+      new Date(hit.created_at_i * 1000),
+      "yyyy-MM-dd",
+      "",
+    ),
   }));
 
   req.respond({ body: JSON.stringify(stories) });
