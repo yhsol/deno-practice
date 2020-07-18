5541597aff9a446b8633df43131475867be6b3ac
loshy <loshy244110@gmail.com>
2020-07-18 21:07:45 +0900

    feat: secure in deno --allow-net
    - fetch hacker news

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello Deno");
+// console.log("Hello Deno");
+
+type StoryT = {
+  title: string;
+  url: string;
+  created_at_i: number;
+};
+
+const url = "http://hn.algolia.com/api/v1/search?query=javascript";
+
+fetch(url).then((result) => result.json()).then((result) => {
+  const stories = result.hits.map((hit: StoryT) => ({
+    title: hit.title,
+    url: hit.url,
+    createdAt: hit.created_at_i,
+  }));
+
+  console.log(stories);
+});
